ddf5411e8f03c196f1e3d9fad695d67680f268ec
Evrim Coskun <evrimcoskun@gmail.com>
2021-01-26 20:10:08 +0100

    delete cache

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://ec-myflix-api.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    })
+  }
+
+  onBackClick() {
+    this.setState({
+      selectedMovie: null
+    })
   }
 
   render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (!movies) return <div className="main-view" />;
+
     return (
-      <div className="main-view"></div>
+      <div className="main-view">
+        {
+          selectedMovie
+            ? <MovieView movie={selectedMovie} onClick={() => this.onBackClick()} />
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+            ))
+        }
+      </div>
     );
   }
 }
